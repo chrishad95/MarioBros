8abbdfea459dbb6482ecf7838e55bd743d4f4a9f
chrishad95 <chrishadley@gmail.com>
2015-10-08 17:43:52 -0400

    end of Part 13

--- core/src/com/taylorbest/mariobros/sprites/Brick.java
+++ core/src/com/taylorbest/mariobros/sprites/Brick.java
@@ -1,8 +1,10 @@
 package com.taylorbest.mariobros.sprites;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.World;
+import com.taylorbest.mariobros.MarioBros;
 
 /**
  * Created by chadley on 10/7/2015.
@@ -11,10 +13,13 @@ public class Brick extends InteractiveTileObject {
     public Brick(World world, TiledMap map, Rectangle bounds) {
         super(world, map, bounds);
         fixture.setUserData(this);
+        setCategoryFilter(MarioBros.BRICK_BIT);
     }
 
     @Override
     public void onHeadHit() {
-
+        Gdx.app.log("Brick", "Collision");
+        setCategoryFilter(MarioBros.DESTROYED_BIT);
+        getCell().setTile(null);
     }
 }
--- core/src/com/taylorbest/mariobros/sprites/Coin.java
+++ core/src/com/taylorbest/mariobros/sprites/Coin.java
@@ -1,9 +1,10 @@
 package com.taylorbest.mariobros.sprites;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.World;
-
+import com.taylorbest.mariobros.MarioBros;
 
 
 /**
@@ -13,10 +14,13 @@ public class Coin extends InteractiveTileObject {
     public Coin(World world, TiledMap map, Rectangle bounds) {
         super(world, map, bounds);
         fixture.setUserData(this);
+        setCategoryFilter(MarioBros.COIN_BIT);
     }
 
     @Override
     public void onHeadHit() {
+        Gdx.app.log("Brick", "Collision");
+        //setCategoryFilter(MarioBros.DESTROYED_BIT);
 
     }
 }
